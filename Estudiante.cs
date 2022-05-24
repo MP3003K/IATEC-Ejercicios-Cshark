be223fa7a414d8f8661686d9f56e8718d173f3dc
Korth <mijailpalomino@upeu.edu.pe>
2022-05-24 07:09:20 -0500

    Agregar archivos de proyecto.

---
+++ Estudiante.cs
@@ -0,0 +1,13 @@
+﻿public class Estudiante
+{
+    public Estudiante(int iD, string nombre)
+    {
+        ID = iD;
+        Nombre = nombre;
+    }
+
+    public int ID { get; private set; }
+    public String Nombre {  get; private set; }
+    
+   
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, World!");
+List<Estudiante> estudiantes = new()
+{
+new Estudiante(1,"Liza"),
+new Estudiante(2,"Ana"),
+new Estudiante(3,"Juana"),
+new Estudiante(4,"Zeno"),
+new Estudiante(4,"Perez"),
+};
+//Buenas tardes
+var ordenadosPorNombre = estudiantes.OrderBy(e => e.Nombre);
+foreach( var estudiante in ordenadosPorNombre)
+{
+    Console.WriteLine($"{estudiante.ID}{estudiante.Nombre}");
+}
